a035bbb1a38e78243ae1d98a4c18d73863104769
Beyluta <beyluta1@gmail.com>
2023-03-25 15:15:16 +0100

    major change style changes

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,8 +7,26 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class SideBarComponent {
   @Output() switchTab = new EventEmitter<string>();
+  @ViewChild('optionsTab') optionsTabElement: any;
+  @ViewChild('serversTab') serversTabElement: any;
 
   switch(tab: string) {
+    this.setActive(tab);
     this.switchTab.emit(tab);
+  }
+
+  setActive(tab: string) {
+    this.optionsTabElement.nativeElement.classList.remove('selected');
+    this.serversTabElement.nativeElement.classList.remove('selected');
+
+    switch (tab) {
+      case 'options':
+        this.optionsTabElement.nativeElement.classList.add('selected');
+        break;
+
+      case 'servers':
+        this.serversTabElement.nativeElement.classList.add('selected');
+        break;
+    }
   }
 }
